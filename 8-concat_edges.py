377b859f3d8762344bb9bd1d4371ec455b106b30
Barkitad <barktadesse@gmail.com>
2022-08-24 10:05:23 -0700

    task

--- 8-concat_edges.py
+++ 8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-str = str[39:66] + str[106:112] + str[:6]
-print(str)chmod u+x 8-concat_edges.py
+str1 = "Holberton"
+str2 = "School"
+str1 += ' ' + str2
+print(f"Welcome to {str1}!")
